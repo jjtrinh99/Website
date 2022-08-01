903462ba34791d108e05c808ceab6b23e33263d4
jjtrinh99 <jontrinh99@gmail.com>
2022-08-01 16:13:55 -0700

    Updated fixes

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <overallh className={styles.title}>
-           <overallh>Jonathan Trinh</overallh>
-        </overallh>
+        <div className={styles.title}>
+           <div className="overallh">Jonathan Trinh</div>
+        </div>
 
   
-           <br /><p className={styles.code}><div>Master&#39;s student studying Computer Engineering &#64; UCI </div>
+           <br /><p className={styles.code}>Master&#39;s student studying Computer Engineering &#64; UCI 
       </p> 
      
-       <p className={styles.description}><div>Nice seein&#39; you out here&#46;<span className={styles.logo}>
-            <Image src="/waving-hand.png" width={72} height={40} alt="emoji"/> </span></div> 
-            <br /> <div>Computer Engineer based in California&#44; graduating in December 2022&#46;
-          Looking into new-grad full-time roles in Software Development Engineering &#40;SDE&#41;&#46;</div>
+       <p className={styles.description}>Nice seein&#39; you out here&#46;<span className={styles.logo}>
+            <Image src="/waving-hand.png" width={72} height={40} alt="emoji"/> </span> 
+            <br />Computer Engineer based in California&#44; graduating in December 2022&#46;
+          Looking into new-grad full-time roles in Software Development Engineering &#40;SDE&#41;&#46;
           
-        <br /><div>Hope you can find more about me&#58; I&#39;ve included my Work + Volunteering Experience&#44; projects worked on&#44; and some hobbies&#46;</div>
+        <br />Hope you can find more about me&#58; I&#39;ve included my Work + Volunteering Experience&#44; projects worked on&#44; and some hobbies&#46;
       </p> 
       
        <br /><h2>Contact Info <span className={styles.logo}>
@@ -65,7 +65,8 @@ export default function Home() {
           <div>
 
             <div  className={styles.card}>
-              <p><title>Project Management Intern</title><div>Lumen Technologies </div><br /> <date>June 2022 - Current</date></p>
+              <p><title>Project Management Intern</title>Lumen Technologies<br /></p>
+              <div className="date">June 2022 - Current</div>
               <br />  
               &#8226;  Study the workflows of various employees around the company including engineers&#44; managers&#44; and contractors&#44; to streamline needed documentation and bottlenecks for convenience&#46;
               <br /><br />&#8226;  Obtain various overviews of the market &amp; projects being worked on to allow for full end-to-end completion of work by needed dates&#46;
@@ -74,8 +75,8 @@ export default function Home() {
             
             <div  className={styles.card}>
               <p><title>IAW Lab Leader &#40;Instructor&#41;</title>
-                <br /><div>Academic Resource Center; <br /> University of California&#44; Riverside
-                <date><br />May 2020 - June 2021</date></div></p>
+                <br />Academic Resource Center; <br /> University of California&#44; Riverside</p>
+                <div className="date"><br />May 2020 - June 2021</div>
                 <br />
                 &#8226;  Leveraged the usage of internet domains to teach intermediate algebra to students&#46;
                 <br /><br />&#8226;  Conducted lab sessions for multiple classes of 20+ students to help grasp new strategies and concepts&#46;
@@ -83,7 +84,8 @@ export default function Home() {
             </div>
             
             <div  className={styles.card}>
-              <p><title>IAW Reader &#40;Grader&#41;</title><br /><div>Academic Resource Center; <br /> University of California&#44; Riverside<date><br />May 2019 - June 2021</date></div></p>
+              <p><title>IAW Reader &#40;Grader&#41;</title><br />Academic Resource Center; <br /> University of California&#44; Riverside</p>
+              <div className="date"><br />May 2019 - June 2021</div>
               
               <br />
               &#8226;  Evaluated and recorded performance of students and assist instructors in grading upwards of 600+ exams&#46;
@@ -93,7 +95,8 @@ export default function Home() {
             </div>
             
             <div  className={styles.card}>
-              <p><title>Research Assistant &#40;Embedded Systems&#41;</title><div> University of California&#44; Riverside; <br /><date>Sept&#46; 2018 - Sept&#46; 2019</date></div><br /></p>
+              <p><title>Research Assistant &#40;Embedded Systems&#41;</title> University of California&#44; Riverside; <br /></p>
+              <div className="date">Sept&#46; 2018 - Sept&#46; 2019</div><br />
               
               <br />&#8226;  Department of CSE &#40;Embedded Systems Lab&#41;&#46; Worked under Dr&#46; Brian Crites &amp; Dr&#46; Philip Brisk&#46;
               <br /><br />&#8226;  Helped research in microfluidic fabrication and integration with electrical systems to aid in automation&#46;
@@ -113,7 +116,8 @@ export default function Home() {
               <div>
 
                 <div  className={styles.card}>
-                  <p><title>Director</title><br /><div> Cutie &amp; Citrus Hack <br /><date>May 2020 - June 2021</date></div><br /></p>
+                  <p><title>Director</title><br /> Cutie &amp; Citrus Hack <br /></p>
+                  <div className="date">May 2020 - June 2021</div><br />
                   <br />
                   &#8226; Led a committee consisting of team leads and subcommittees&#44; inclusive of Sponsorship&#44; Web Development&#44; Operations&#44; &amp; Marketing&#46;
                   <br /><br />&#8226; Streamlined and centralized workflow towards an impactful hackathon through documentation&#44; inclusivity&#44; engagement&#44; and collaboration&#46;
@@ -121,14 +125,16 @@ export default function Home() {
                 </div>
 
                 <div  className={styles.card}>
-                  <p><title>Sponsorship Lead</title><br /><div> Cutie &amp; Citrus Hack <br /><date>May 2019 - May 2020</date></div><br /></p>
+                  <p><title>Sponsorship Lead</title><br /> Cutie &amp; Citrus Hack <br /><br /></p>
+                  <div className="date">May 2019 - May 2020</div>
                   <br />
                   &#8226; Raised upwards of $20&#44;000 to fund Citrus Hack&#44; a 36-hour hackathon&#44; along with Cutie Hack&#44; a 12-hour sister hackathon&#46;
                   <br /><br />&#8226;  Sponsors included GCP&#44; Sketch&#44; &amp; WolframAlpha&#46;
                 </div>
 
                 <div  className={styles.card}>
-                  <p><title>Secretary</title><br /><div> Materials Research Society &#40;MRS&#41;<br /><date>June 2019 - June 2020</date></div><br /></p>
+                  <p><title>Secretary</title><br /> Materials Research Society &#40;MRS&#41;<br /></p>
+                  <div className="date">June 2019 - June 2020</div><br />
                   <br />
                   &#8226;  Maintained records of meeting notes and meeting minutes&#44; along with various paperwork and forms&#46;
                   <br /><br />&#8226; Managed emails&#44; summarize contents meetings and events for members and club participants
@@ -136,7 +142,8 @@ export default function Home() {
                 </div>
 
                 <div  className={styles.card}>
-                  <p><title>Historian</title><br /><div> Materials Research Society &#40;MRS&#41;<br /><date>Sept&#46; 2018 - June 2019</date></div><br /></p>
+                  <p><title>Historian</title><br /> Materials Research Society &#40;MRS&#41;<br /></p>
+                  
                   <br />
                   &#8226;  Took and uploaded pictures for events and meetings hosted
                   <br /><br />&#8226; Worked with outreach chair &amp; promote events through social media /w pictures and advertisement
@@ -153,20 +160,22 @@ export default function Home() {
           >
             <h2>Projects &amp; Work Done</h2>
         
-          <div>
+          
 
             <a href="https://jonathantrinh.vercel.app/" target="_blank" rel="noreferrer" className={styles.card}>
-              <p><title>Personal Site &rarr; </title><date><br />Aug. 2022 - Current</date></p>
+              <p><title>Personal Site &rarr; </title></p>
+              <div className="date"><br />Aug. 2022 - Current</div>
               <br />&#8226; Making my personal site&#46; Working on adding features to make the site look better&#33;
               <br /><br /> &#8226; Built with React&#44; NextJS&#44; and deployed through Vercel&#46;
               <br /><br /> &#8226; Special shoutout to Rajbir Johar&#44; Danial Beg&#44; and Suhas Jagannath&#46;
             </a>
          
-          </div>
+          
 
-            <a href="https://docs.google.com/document/d/1Xkci0hA98UrtymjMoVoA1M5H1dRsxnQ0aHoMEtcKdek/edit?usp=sharing" target="_blank" rel="noreferrer" className={styles.card}>
+          <a href="https://docs.google.com/document/d/1Xkci0hA98UrtymjMoVoA1M5H1dRsxnQ0aHoMEtcKdek/edit?usp=sharing" target="_blank" rel="noreferrer" className={styles.card}>
         
-              <p><title>Smart Vase IoT &rarr;</title><date><br /> Nov&#46; 2021 - Dec&#46; 2021</date></p>
+              <p><title>Smart Vase IoT &rarr;</title></p>
+              <div className="date"><br /> Nov&#46; 2021 - Dec&#46; 2021</div>
               
               <br />&#8226; Created an IoT Device&#46; Tracks a vase via smart monitoring to extend the shelf life of flowers&#46;
               <br /><br />&#8226; Monitors water levels of vase&#44; temperature&#44; humidity&#44; and light levels of environment&#46;
@@ -174,8 +183,9 @@ export default function Home() {
           
           </a>
 
-            <a href="hhttps://docs.google.com/document/d/1K9Kk7v5S7woE3pdP6iqIU7xLfaW-thwBnI_xWZO0x8k/edit" target="_blank" rel="noreferrer" className={styles.card}>
-            <p> <title>Smart Pillow IoT &rarr;</title><date><br /> Sept&#46; 2021 - June 2021</date></p>
+          <a href="hhttps://docs.google.com/document/d/1K9Kk7v5S7woE3pdP6iqIU7xLfaW-thwBnI_xWZO0x8k/edit" target="_blank" rel="noreferrer" className={styles.card}>
+            <p> <title>Smart Pillow IoT &rarr;</title></p>
+            <div className="date"><br /> Sept&#46; 2021 - June 2021</div>
             <br />&#8226; Senior Design in Embedded Systems&#46;
             <br /><br />&#8226; Developed a Smart Pillow geared towards Health IoT &amp; Embedded Systems&#44; specifically through the creation of a Smart Device&#46; 
             <br /><br />&#8226; Includes mechanical actuation&#44; temperature heating &amp; cooling of the pillow&#44; heart rate sensing&#44; and snoring detection&#46;
@@ -184,23 +194,26 @@ export default function Home() {
           </a>
           
 
-            <a href="https://docs.google.com/document/d/1pYLFrX4qAp_fLJdlanWYrLAlOdWDGDz_FReZX_Z_H0Q/edit" target="_blank" rel="noreferrer" className={styles.card}>
-            <p><title>Meet The Earth &rarr; </title><date><br /> July 2021 - Sept&#46; 2021</date></p> 
+          <a href="https://docs.google.com/document/d/1pYLFrX4qAp_fLJdlanWYrLAlOdWDGDz_FReZX_Z_H0Q/edit" target="_blank" rel="noreferrer" className={styles.card}>
+            <p><title>Meet The Earth &rarr; </title></p> 
+            <div className="date"><br /> July 2021 - Sept&#46; 2021</div>
             <br />&#8226; iOS App Development. Implemented on XCode 11 with Swift 5&#46;1
             <br /><br />&#8226; Collaborated with and led a team to develop an iOS App&#46; Github included in the technical report&#46;
             <br /><br />&#8226; Geared towards a user&#39;s health and fitness&#44; specifically in powerlifting&#46;
             <br /><br />&#8226; Utilizes CocoaPads and Firebase to implement Calendar Framework &amp; RealTime Database&#46;
           </a>
 
-            <a href="https://docs.google.com/presentation/d/120sBDQv1Sr-K6onshXQiYKaA25wh7cVHcz5I15wKU9Q/edit#slide=id.p" target="_blank" rel="noreferrer" className={styles.card}>
-            <p><title>Low Cost PCR on Microfluidic Device &rarr;</title><date><br />Sept 2018 - Sept&#46; 2019</date></p>
+          <a href="https://docs.google.com/presentation/d/120sBDQv1Sr-K6onshXQiYKaA25wh7cVHcz5I15wKU9Q/edit#slide=id.p" target="_blank" rel="noreferrer" className={styles.card}>
+            <p><title>Low Cost PCR on Microfluidic Device &rarr;</title></p>
+            <div className="date"><br />Sept 2018 - Sept&#46; 2019</div>
               <br />&#8226;Designed microfluidic chips to create a prototypical PCR chip and a functional environment to perform PCR
               <br /><br />&#8226;  Created the initial microfluidic device based on a sample PCR assay&#46;
               <br /><br />&#8226;  Participated in initial wet and dry testing to validate device integrity and heating ability&#46;
           </a>
 
-            <a href="https://docs.google.com/document/d/155uo9RAQhZOzTAOQ9Ve1RDytzQzga8hz/edit?usp=sharing&amp;ouid=109920275224725165600&amp;rtpof=true&amp;sd=true" target="_blank" rel="noreferrer" className={styles.card}>
-           <p><title>Smart Street Intersection &rarr;</title><date><br /> May 2019 - June 2019</date></p> 
+          <a href="https://docs.google.com/document/d/155uo9RAQhZOzTAOQ9Ve1RDytzQzga8hz/edit?usp=sharing&amp;ouid=109920275224725165600&amp;rtpof=true&amp;sd=true" target="_blank" rel="noreferrer" className={styles.card}>
+            <p><title>Smart Street Intersection &rarr;</title></p> 
+            <div className="date"><br /> May 2019 - June 2019</div>
             <br />&#8226;  Project in Embedded Systems
             <br /><br />&#8226;  Used an Atmega1284&#46; Programmed with C to communicate between various Piezo and IR Sensors&#46;
             <br /><br />&#8226;  Used various sensors and LEDs to display who is in the intersection&#44; helping to simulate a more efficient &amp; energy saving infrastructure&#46;
